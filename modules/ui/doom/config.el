a5209f820f27833f59b9ea86d50303841326d34c
Liam Hupfer <liam@hpfr.net>
2023-09-23 23:32:01 -0500

    tweak(ui): remove pos-tip configuration
    pos-tip was removed five years ago in afb92a8c7f46.

--- modules/ui/doom/config.el
+++ modules/ui/doom/config.el
@@ -1,10 +1,5 @@
 ;;; ui/doom/config.el -*- lexical-binding: t; -*-
 
-;;;###package pos-tip
-(setq pos-tip-internal-border-width 6
-      pos-tip-border-width 1)
-
-
 (use-package! doom-themes
   ;; improve integration w/ org-mode
   :hook (doom-load-theme . doom-themes-org-config)
