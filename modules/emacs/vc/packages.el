9d5a809f28e9edb4a8ad3c1118ffc3f33f96a52f
Ben Chen <517926804@qq.com>
2024-03-20 09:18:30 +0800

    bump: :emacs vc

--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -6,9 +6,9 @@
 (package! smerge-mode :built-in t)
 
 (package! browse-at-remote :pin "76aa27dfd469fcae75ed7031bb73830831aaccbf")
-(package! git-commit :pin "65ecb9c5fc7586a1c527b60d180a97ea230da99f")
+(package! git-commit :pin "0963697f24cfbe80f92312044bd9ab28b914b053")
 (package! git-timemachine
   ;; The original lives on codeberg.org; which has uptime issues.
   :recipe (:host github :repo "emacsmirror/git-timemachine")
-  :pin "ac933e5cd29583c131401f3bd991d98129c316df")
+  :pin "5ed73c3831cf6da10ba941e6abba708a86853e8f")
 (package! git-modes :pin "3cc94974c09c43462dfbfbe20396a414352dbb92")
