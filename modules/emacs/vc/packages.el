9e76fc491fa2429436c62eb43ce5d96b980c385e
Ben Chen <517926804@qq.com>
2024-07-16 11:43:58 +0800

    bump: :emacs vc
    magit/git-modes@7901415b39be -> magit/git-modes@d96fa7a3c7d7
    magit/magit@9d1f8db507e0 -> magit/magit@b9948f957192

--- modules/emacs/vc/packages.el
+++ modules/emacs/vc/packages.el
@@ -6,9 +6,9 @@
 (package! smerge-mode :built-in t)
 
 (package! browse-at-remote :pin "76aa27dfd469fcae75ed7031bb73830831aaccbf")
-(package! git-commit :pin "9d1f8db507e080e032943a3ed1445bd8d9aaa9fc")
+(package! git-commit :pin "b9948f9571928bb7f39f4b3a112bd76e52a072ce")
 (package! git-timemachine
   ;; The original lives on codeberg.org; which has uptime issues.
   :recipe (:host github :repo "emacsmirror/git-timemachine")
   :pin "3780835fcd67c3703ffa768206121851e6895ece")
-(package! git-modes :pin "7901415b39bef1b789d161ca2c10dfb910d13780")
+(package! git-modes :pin "d96fa7a3c7d754812675b37247c6a77e459eec53")
