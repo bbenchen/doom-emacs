9f657c37b3deea28c55cadfa190b7997f92bd623
Ben Chen <517926804@qq.com>
2025-09-04 09:54:29 +0800

    Revert "chore(lang): improve python and yaml tree-sitter config"
    This reverts commit 2a2c4c890b7c8d1dca511cc7af39a0e6387beb42.

--- modules/lang/yaml/config.el
+++ modules/lang/yaml/config.el
@@ -12,7 +12,9 @@
   :when (modulep! +tree-sitter)
   :defer t
   :init
-  (set-tree-sitter! 'yaml-mode 'yaml-ts-mode)
+  (set-tree-sitter! 'yaml-mode 'yaml-ts-mode
+    '((yaml :url "https://github.com/tree-sitter-grammars/tree-sitter-yaml"
+            :commit "b733d3f5f5005890f324333dd57e1f0badec5c87")))
   :config
   (when (modulep! +lsp)
     (add-hook 'yaml-ts-mode-local-vars-hook #'lsp! 'append)))
